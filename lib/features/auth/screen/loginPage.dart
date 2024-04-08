8fc71498c510f19fa4075857a42373bff4fa0292
shabe <SHABEEBK246@GMAIL.COM>
2024-04-08 14:01:04 +0530

    videoplayer

--- lib/features/auth/screen/loginPage.dart
+++ lib/features/auth/screen/loginPage.dart
@@ -16,6 +16,8 @@ import 'package:luna_demo/features/home/screen/navbar.dart';
 import 'package:luna_demo/main.dart';
 import 'package:luna_demo/model/user_Model.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
 
 class LoginPage extends ConsumerStatefulWidget {
   const LoginPage({super.key});
@@ -35,162 +37,195 @@ class _LoginPageState extends ConsumerState<LoginPage> {
     ref.watch(googlesignProv).signInWithGoogle(context);
   }
 
+  final VideoPlayerController videoPlayerController = VideoPlayerController.asset('assets/video/cat1.mp4',);
+
+  ChewieController ? chewieController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    chewieController = ChewieController(videoPlayerController: videoPlayerController,
+    aspectRatio: width*1/height*1,
+      autoPlay: true,
+      looping: true,
+      autoInitialize: true,
+      showControls: false,
+
+
+    );
+
+    @override
+    void dispose() {
+      videoPlayerController.dispose();
+      chewieController!.dispose();
+      super.dispose();
+    }
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(width * 0.03),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(imageConstants.pageimage),
-              fit: BoxFit.cover,
-              opacity: width * 0.00009),
-        ),
-        child: Column(
-          children: [
-            Container(
-              height: height * 0.15,
-              width: width * 1,
-              margin: EdgeInsets.only(top: width * 0.3),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    imageConstants.dogSvg,
-                    width: width * 0.08,
-                    height: height * 0.08,
-                  )
-                ],
-              ),
-            ),
-            Text(
-              "Let's Get Started!",
-              style: TextStyle(
-                  fontSize: width * 0.07, fontWeight: FontWeight.w900),
-            ),
-            Text(
-              "Let's dive into your account",
-              style: TextStyle(
-                  fontSize: width * 0.04, color: Pallette.primaryColor),
-            ),
-            gap,
-            Container(
-              height: height * 0.3,
-              width: width * 1,
-              // color: Colors.yellow,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      functionGoogle();
-                    },
-                    child: Container(
-                      width: width * 1,
-                      height: height * 0.065,
-                      margin: EdgeInsets.all(
-                        width * 0.05,
-                      ),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(width * 0.1),
-                          border: Border.all(color: Pallette.grey)),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Image(
-                              image: AssetImage(imageConstants.googleicon),
-                              height: width * 0.07,
-                              width: width * 0.07),
-                          Text("Continue with Google"),
-                          SizedBox()
-                        ],
-                      ),
-                    ),
+      body: Stack(
+        children: [
+          Expanded(child: Chewie(controller: chewieController!)),
+          Container(
+            // padding: EdgeInsets.all(width * 0.03),
+            // decoration: BoxDecoration(
+            //   image: DecorationImage(
+            //       image: AssetImage(imageConstants.pageimage),
+            //       fit: BoxFit.cover,
+            //       opacity: width * 0.00009),
+            // ),
+            child: Column(
+              children: [
+                Container(
+                  height: height * 0.15,
+                  width: width * 1,
+                  margin: EdgeInsets.only(top: width * 0.3),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        imageConstants.dogSvg,
+                        width: width * 0.08,
+                        height: height * 0.08,
+                      )
+                    ],
                   ),
-                  Container(
-                    width: width * 1,
-                    height: height * 0.06,
-                    margin: EdgeInsets.only(
-                      left: width * 0.04,
-                      right: width * 0.04,
-                    ),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(width * 0.1),
-                        border: Border.all(color: Pallette.grey)),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Image(
-                            image: AssetImage(imageConstants.appleicon),
-                            height: width * 0.07,
-                            width: width * 0.07),
-                        Text("Continue with Apple"),
-                        SizedBox()
-                      ],
-                    ),
+                ),
+                Text(
+                  "Let's Get Started!",
+                  style: TextStyle(
+                      fontSize: width * 0.07, fontWeight: FontWeight.w900,color: Pallette.white),
+                ),
+                Text(
+                  "Let's dive into your account",
+                  style: TextStyle(
+                      fontSize: width * 0.04, color: Pallette.white,),
+                ),
+                gap,
+                Container(
+                  height: height * 0.3,
+                  width: width * 1,
+                  // color: Colors.yellow,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          functionGoogle();
+                        },
+                        child: Container(
+                          width: width * 1,
+                          height: height * 0.065,
+                          margin: EdgeInsets.all(
+                            width * 0.05,
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                              borderRadius: BorderRadius.circular(width * 0.1),
+                              ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image(
+                                  image: AssetImage(imageConstants.googleicon),
+                                  height: width * 0.07,
+                                  width: width * 0.07),
+                              Text("Continue with Google"),
+                              SizedBox()
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: width * 1,
+                        height: height * 0.065,
+                        margin: EdgeInsets.only(
+                          left: width * 0.04,
+                          right: width * 0.04,
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(width * 0.1),
+                            color: Colors.grey),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Image(
+                                image: AssetImage(imageConstants.appleicon),
+                                height: width * 0.07,
+                                width: width * 0.07),
+                            Text("Continue with Apple"),
+                            SizedBox()
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                gap,
+                Container(
+                  height: height * 0.15,
+                  width: width * 1,
+                  // color: Colors.tealAccent,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) => SignupPage(
+                                  sign: false,
+                                ),
+                              ));
+                        },
+                        child: Container(
+                          height: height * 0.05,
+                          width: width * 0.6,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(width * 0.1),
+                            color: Pallette.primaryColor,
+                          ),
+                          child: Center(
+                              child: Text(
+                                "Sign up",
+                                style: TextStyle(color: Pallette.white),
+                              )),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) => SigninPage(),
+                              ));
+                        },
+                        child: Container(
+                          height: height * 0.05,
+                          width: width * 0.6,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(width * 0.1),
+                            color: Pallette.primaryColor,
+                          ),
+                          child: Center(
+                              child: Text(
+                                "Sign in",
+                                style: TextStyle(color: Pallette.white),
+                              )),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-            gap,
-            Container(
-              height: height * 0.15,
-              width: width * 1,
-              // color: Colors.tealAccent,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => SignupPage(
-                              sign: false,
-                            ),
-                          ));
-                    },
-                    child: Container(
-                      height: height * 0.05,
-                      width: width * 0.6,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(width * 0.1),
-                        color: Pallette.primaryColor,
-                      ),
-                      child: Center(
-                          child: Text(
-                        "Sign up",
-                        style: TextStyle(color: Pallette.white),
-                      )),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          CupertinoPageRoute(
-                            builder: (context) => SigninPage(),
-                          ));
-                    },
-                    child: Container(
-                      height: height * 0.05,
-                      width: width * 0.6,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(width * 0.1),
-                        color: Pallette.primaryColor,
-                      ),
-                      child: Center(
-                          child: Text(
-                        "Sign in",
-                        style: TextStyle(color: Pallette.white),
-                      )),
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
